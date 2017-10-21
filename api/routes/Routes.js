85917262f7e994d8dbea592fe93a40f942bec304
julien pons <julien.pons.gillot03@gmail.com>
2017-10-21 11:39:11 +0200

    documentation

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,11 +1,20 @@
 'use strict'
 import gifService from '../controllers/GifController.js'
 import paths from '../conf/Paths'
+import Joi from 'joi'
 
 module.exports = (server) => {
     server.route({
-      method: 'GET',
-      path: paths.intern.getRandomGif(),
-      handler: gifService.random_gif
+        method: 'GET',
+        path: paths.intern.getRandomGif(),
+        config: {
+            tags: ['api'],
+            validate: {
+                params: {
+                    tag: Joi.string().description('tag')
+                }
+            },
+            handler: gifService.random_gif
+        }
     })
 }
--- index.js
+++ index.js
@@ -2,14 +2,43 @@
 
 import Hapi from 'hapi'
 require('dotenv').config()
+import swaggered from 'hapi-swaggered'
+import swaggeredUI from 'hapi-swaggered-ui'
+import vision from 'vision'
+import inert from 'inert'
 
 if (!process.env.GIPHY_TOKEN && !process.env.PATH) {
   throw 'Make sure you defined GIPHY_TOKEN and PATH in your .env file'
 }
 
 const server = new Hapi.Server()
-server.connection({ port: 4322, host: '192.168.100.1', routes: { cors: true } })
-//server.connection({ port: 4322, host: '192.168.43.20', routes: { cors: true } })
+//server.connection({ port: 4322, host: '192.168.100.1', routes: { cors: true } })
+server.connection({ port: 4322, host: '192.168.43.20', routes: { cors: true } , labels: ['api'] })
+
+server.register([
+    vision,
+    inert,
+    {
+        register: swaggered,
+        options: {
+            info: {
+                title: 'Gif Service API',
+                description: 'API documentation for Gif Service',
+                version: '1.0'
+            }
+        }
+    },
+    {
+        register: swaggeredUI,
+        options: {
+            title: 'Gif Bot Gif-Service API',
+            path: '/docs',
+            swaggerOptions: {}
+        }
+    }
+], {
+    select: 'api'
+}, (err) => { if (err) { throw err } })
 
 const routes = require('./api/routes/Routes')
 routes(server)
